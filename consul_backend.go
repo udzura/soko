d01291d79692a392d23ed593f7f28079abb52f0a
Uchio KONDO <udzura@pepabo.com>
2015-07-01 17:53:17 +0900

    Implement interface method

--- consul_backend.go
+++ consul_backend.go
@@ -1,6 +1,9 @@
 package metama
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/hashicorp/consul/api"
 )
 
@@ -15,4 +18,39 @@ func NewConsulBackend() *ConsulBackend {
 	return &ConsulBackend{
 		client: kv,
 	}
+}
+
+func (b *ConsulBackend) pathOf(serverID string, key string) {
+	const prefix = "metadata"
+	return fmt.Sprintf("%s/%s/%s", prefix, serverID, string)
+}
+
+func (b *ConsulBackend) Save() error {
+	fmt.Fprintf(os.Stderr, "Currently do nothing.")
+	return nil
+}
+
+func (b *ConsulBackend) Get(serverID string, key string) (string, error) {
+	p, _, err := b.client.Get(b.pathOf(serverID, key), nil)
+	if err != nil {
+		return err
+	}
+
+	return string(p.Value), nil
+}
+
+func (b *ConsulBackend) Put(serverID string, key string, value string) error {
+	data := &api.KVPair{
+		Key:   b.pathOf(serverID, key),
+		Value: []byte(value),
+	}
+	_, err := b.client.Put(p, nil)
+
+	return err
+}
+
+func (b *ConsulBackend) Delete(serverID string, key string) error {
+	_, err := b.client.Delete(b.pathOf(serverID, key), nil)
+
+	return err
 }
